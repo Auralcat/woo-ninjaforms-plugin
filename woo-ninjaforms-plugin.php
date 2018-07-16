2330ea89f24bec16041cb5a48f25aab5dea76e7b
Auralcat <ashleyzt@hi2.in>
2018-07-16 14:29:58 -0300

    In the beginning there was darkness

---
+++ woo-ninjaforms-plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Woo Ninjaforms Plugin
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Show WooCommerce orders with NinjaForms submissions
+ * Author:          Miriam Retka
+ * Author URI:      miriamretka.com
+ * Text Domain:     woo-ninjaforms-plugin
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Woo_Ninjaforms_Plugin
+ */
+
+// Your code starts here.
